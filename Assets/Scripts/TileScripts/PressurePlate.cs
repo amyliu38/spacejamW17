6e318f8f98330e59894836b2a08a2a63a5d3b084
Amy Liu <amyliu38@gmail.com>
2017-01-14 11:46:51 -0500

    added 2nd level and tile classes

---
+++ Assets/Scripts/TileScripts/PressurePlate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Physics.Raycast (transform.position, transform.up, 5f)) {
+			//trigger some function, door or drawbridge etc
+		}
+	}
+}
