1d359035cdde67df21c2856a6c894cd7bb4c2b76
Amy Liu <amyliu38@gmail.com>
2017-01-14 00:44:39 -0500

    added level load code

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour {
+
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision coll){
+		//send message to pause player movement
+		coll.transform.parent = transform;
+		anim.SetTrigger ("Rise");
+	}
+
+	void LoadNextLevel(){
+		int levelIdx = SceneManager.GetActiveScene ().buildIndex;
+		SceneManager.LoadScene (++levelIdx);
+	}
+
+
+}
