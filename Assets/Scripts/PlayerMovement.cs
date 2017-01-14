774ede5c9134ced08e39eaec4680939a485fb750
BitMasterZ <adlakhashobhit@gmail.com>
2017-01-14 12:39:27 -0500

    Changed the player movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -75,7 +75,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetButtonDown("Jump") && charController.isGrounded)
         {
-            print("Jump");
+            //print("Jump");
             movement.y = jumpSpeed;
 
         }
