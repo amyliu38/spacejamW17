027813ffe8efa902d54fa1082d8ee6435c492d6d
Michael Zhang <zhamicha@umich.edu>
2017-01-14 14:05:33 -0500

    Commiting moving platforms:snowflake:

---
+++ Assets/Scripts/PlatformMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMove : MonoBehaviour {
+
+    Animator anim;
+    public bool moveRL = false;
+    public bool moveUD = false;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (moveRL)
+        {
+            anim.SetTrigger("MoveRL");
+        }
+        if (moveUD)
+        {
+            anim.SetTrigger("MoveUD");
+        }
+	}
+}
